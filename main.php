d97b3fad44f2b5b27d3590f8ee47ac7192aea662
Juliana Gagliano <julianagagliano1@gmail.com>
2024-03-13 23:31:09 -0300

    feat(main): starting main sidebar - front

--- main.php
+++ main.php
@@ -4,9 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="assets/crvital-logo.svg" type="image/x-icon">
+    <link rel="stylesheet" href="main-sidebar.css">
     <title>CrVital - Página Inicial</title>
 </head>
 <body>
-    <h1>Página da Recepcionista - Teste</h1>
+    <div class="sidebar">
+        <img class="img-logo" src="assets/crvital-logo.svg">
+        <ul>
+            <li><a href="#"><img class="icons-main" src="assets/user-icon.png">Usuário</a></li>
+            <li><a href="#"><img class="icons-main" src="assets/attendance-icon.png">Atendimento</a></li>
+            <li><a href="#">Empresas</a></li>
+            <li><a href="#">Exames</a></li>
+            <li><a href="#">Médicos</li>
+            <li><a href="#">Relatório</li>
+        </ul>
+        <img class="logout-icon" src="assets/logout-icon.png">
+    </div>
+        
+    <!-- <div class="content">
+        <div id="aba1">Conteúdo da Aba 1</div>
+        <div id="aba2">Conteúdo da Aba 2</div>
+        <div id="aba3">Conteúdo da Aba 3</div>
+        <div id="aba4">Conteúdo da Aba 4</div>
+        <div id="aba5">Conteúdo da Aba 5</div>
+    </div>  -->
+
 </body>
 </html>
