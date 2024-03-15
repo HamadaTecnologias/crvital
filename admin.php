d3e349993d83b3000dcad5d2f39474edb4d15129
Juliana Gagliano <julianagagliano1@gmail.com>
2024-03-14 23:53:14 -0300

    feat(admin): start admin page - front

--- admin.php
+++ admin.php
@@ -4,9 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="assets/crvital-logo.svg" type="image/x-icon">
+    <link rel="stylesheet" href="main-sidebar.css">
+    <link rel="stylesheet" href="admin.css">
     <title>CrVital - Página do Administrador</title>
 </head>
 <body>
-    <h1>Página do Admin - Teste</h1>
+    <section class="flex">
+    <div class="sidebar">
+        <a href="logout.php"><img class="logout-icon" src="assets/logout-icon.png"></a>
+        <img class="img-logo" src="assets/crvital-logo.svg">
+        <ul>
+            <li><a href="#"><img class="icons-main" src="assets/user-icon.png">Usuários</a></li>
+            <li><a href="#"><img class="icons-main" src="assets/calendar.png">Atendimentos</a></li>
+            <li><a href="#"><img class="icons-main" src="assets/company.png">Empresas</a></li>
+            <li><a href="#"><img class="icons-main" src="assets/stetoscope.png">Exames</a></li>
+            <li><a href="#"><img class="icons-main" src="assets/doctor.png">Médicos</li>
+            <li><a href="#"><img class="icons-main" src="assets/report.png">Relatórios</li>
+        </ul>
+    </div>
+
+    <h3>teste</h3>
+
+    </section>
 </body>
 </html>
