9902568954bcff5c8d6efbac80c5e13dc9ee7e32
Hamada Tecnologias <hamada.st@hotmail.com>
2024-03-15 13:41:25 -0300

    fix: alteracao-bloquear-liberar

--- bloquear.php
+++ bloquear.php
@@ -1,6 +1,18 @@
 <?php 
 
+    include 'bd_connect.php';
 
+
+    $id_empresa = $_GET['id_empresa'];
+
+    $query = "update empresa set status = false where id_empresa =".$id_empresa;
+    if(mysqli_query($con,$query)){
+        echo "Empresa bloqueada com sucesso";
+    }else{
+        echo "Erro ao bloquear Emrpresa: <br>";
+        echo mysqli_error($con);
+        echo "<br>Contate o Suporte";
+    }  
 
 
 ?>
