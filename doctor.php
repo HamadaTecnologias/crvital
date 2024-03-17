87ad6ebf5dca65db9b54d136d4bfa0c81ad13ceb
Juliana Gagliano <julianagagliano1@gmail.com>
2024-03-17 11:37:18 -0300

    feat(backend): starting backend logic in medical page

--- doctor.php
+++ doctor.php
@@ -24,26 +24,60 @@
         </div>
 
     <div class="middle">
-    <div class="new-user">
+    <div class="new-doctor">
         <h3>Cadastrar Novo Médico:</h3>
-        <form action="" method="post">
-            <label for="new_doctor_username">Médico:</label><br>
-            <input type="text" name="new_doctor_username" placeholder="Digite o nome"><br>
+        <form action="new_doctor.php" method="post">
+            <label class="new-doctor-label" for="new_doctor_username">Médico:</label><br>
+            <input class="new-doctor-input" type="text" name="new_doctor_username" placeholder="Digite o nome"><br>
 
-            <label for="new_doctor_cpf">CPF:</label><br>
-            <input type="text" name="new_doctor_cpf" placeholder="Digite o CPF"><br>
+            <label class="new-doctor-label" for="new_doctor_cpf">CPF:</label><br>
+            <input class="new-doctor-input" type="text" name="new_doctor_cpf" placeholder="Digite o CPF"><br>
 
-            <label for="new_doctor_nis">NIS:</label><br>
-            <input type="text" name="new_doctor_nis" placeholder="Digite o número do NIS"><br>
+            <label class="new-doctor-label" for="new_doctor_nis">NIS:</label><br>
+            <input class="new-doctor-input" type="text" name="new_doctor_nis" placeholder="Digite o número do NIS"><br>
 
-            <label for="new_doctor_board">Conselho (SIGLA):</label><br>
-            <input type="text" name="new_doctor_board" placeholder="Digite a sigla do conselho"><br>
+            <label class="new-doctor-label" for="new_doctor_board">Conselho (SIGLA):</label><br>
+            <input class="new-doctor-input" type="text" name="new_doctor_board" placeholder="Digite a sigla do conselho"><br>
 
-            <label for="new_doctor_category">Categoria</label><br>
-            <input type="text" name="new_doctor_category" placeholder="Digite a categoria"><br>
+            <label class="new-doctor-label" for="new_doctor_register_board">Registro no Conselho</label><br>
+            <input class="new-doctor-input" type="text" name="new_doctor_register_board" placeholder="Digite o registro do conselho"><br>
+
+            <label class="new-doctor-label" for="new_doctor_category">Categoria</label><br>
+            <input class="new-doctor-input" type="text" name="new_doctor_category" placeholder="Digite a categoria"><br>
 
             <button type="submit">Cadastrar</button>
         </form>
+    </div>
+
+    <div class="registred-doctors">
+    <h3>Médicos Cadastrados:</h3>
+    <table>
+        <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>CPF</th>
+            <th>Categoria</th>
+            <th>Ações</th>
+        </tr>
+        <?php
+        include 'bd_connect.php';
+        $query = "SELECT id_medico, nome_medico, cpf, categoria FROM medico;";
+        $result = mysqli_query($con, $query);
+        if ($result) {
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['id_medico'] . "</td>";
+                echo "<td>" . $row['nome_medico'] . "</td>";
+                echo "<td>" . $row['cpf'] . "</td>";
+                echo "<td>" . $row['categoria'] . "</td>";
+                echo "<td><a href='delete_doctor.php?id=".$row['id_medico']."'>Excluir</a></td>";
+                echo "</tr>";
+            }
+        } else {
+            echo "Erro ao recuperar os usuários.";
+        }
+        ?>
+    </table>
     </div>
     </div>
     </section>
