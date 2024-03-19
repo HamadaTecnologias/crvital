04f21a7cc547a8c9f8ae2ad7dd8dbe2059766c89
HamadaTecnologias <hamada.st@hotmail.com>
2024-03-19 00:24:30 -0300

    feat: front-empresa-finalizado

--- cadastro_empresas/liberar.php
+++ cadastro_empresas/liberar.php
@@ -5,7 +5,7 @@
     $query = "update empresa set status = true where id_empresa =".$id_empresa;
 
     if(mysqli_query($con,$query)){
-        echo "Empresa liberada com sucesso";
+        header('location:pagina_principal.php?liberado=true');
     }else{
         echo "Erro ao liberar Emrpresa: <br>";
         echo mysqli_error($con);
