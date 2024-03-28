8b45ff24fc12db5eed73b5e34569a1e0436f01b0
Hamada Tecnologias <hamada.st@hotmail.com>
2024-03-28 16:26:49 -0300

    feat: começo_relatorio_periodo

--- bd_connect.php
+++ bd_connect.php
@@ -16,3 +16,11 @@
         exit();
       }
 ?>
+
+
+
+<!-- $query = "SELECT A.data,A.nome_paciente,P.nome_procedimento,P.valor,E.nome_empresa,E.perfil,E.cnpj FROM atendimento A
+        INNER JOIN atendimento_procedimento AP ON A.id_atendimento=AP.id_atendimento
+        INNER JOIN procedimento P ON  P.id_procedimento=AP.id_procedimento
+        INNER JOIN empresa E ON E.id_empresa=E.id_empresa
+        WHERE E.id_empresa =".$id_empresa."AND A.data BETWEEN '".$data_inicio."' and '".$data_fim."'"; -->
--- relatorio/relatorio_periodo.php
+++ relatorio/relatorio_periodo.php
@@ -5,48 +5,80 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="script.js" defer></script>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../assets/css/relatorio_periodo.css">
     <title>Document</title>
 </head>
 <body>
     <div id="content">
 
-            <?php 
-            include "../bd_connect.php";
-            $data_inicio = $_GET['data_inicio'];
-            $data_fim = $_GET['data_fim'];
-            ?>
+    <?php 
+        include "../bd_connect.php";
+        
+        $data_inicio = $_GET['data_inicio'];
+        $data_fim = $_GET['data_fim'];
+        $erro = FALSE;
+        if(empty($data_inicio)){
+            $erro=TRUE;
+            echo $data_inicio;
+        }
+        if(empty($data_fim)){
+            $erro=TRUE;
+            echo $data_fim;
+        }
+        if ($erro != FALSE) {
+            echo"Confira as datas selecionadas";
+        }
+
+        $query_hora="SELECT TIME_FORMAT(CURTIME(), '%H:%i')";
+        $result = mysqli_query($con,$query_hora);
+        $hora = mysqli_fetch_assoc($result);
+        $hora_emissao = $hora["TIME_FORMAT(CURTIME(), '%H:%i')"];
+       
+        //criando array com os ids das empresas
+        $query_valid="select id_empresa from empresa";
+        $empresa = mysqli_query($con,$query_valid);
+        $ids = array();
+        while($linha = mysqli_fetch_assoc($empresa)){
+            array_push($ids,$linha['id_empresa']);
+        }
+
+        
+    ?>
             <table>
                 <thead>
                     <tr>
-                    <th scope="col">Empresa</th>
                     <th scope="col">Colaborador</th>
-                    <th scope="col">Médico Atendente</th>
-                    <th scope="col">Exame</th>
-                    <th scope="col">Check-in</th>
-                    <th scope="col">Finalizar</th>
+                    <th scope="col">Procedimento</th>
+                    <th scope="col">Valor</th>
+                    <th scope="col">Realizado em</th>
+                    <th scope="col">Empresa</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <?php while($consulta = $_SESSION){ ?>
+                    <?php 
+                        $query = "SELECT A.data,A.nome_paciente,P.nome_procedimento,P.valor,E.nome_empresa,E.perfil,E.cnpj FROM atendimento A
+                        INNER JOIN atendimento_procedimento AP ON A.id_atendimento=AP.id_atendimento
+                        INNER JOIN procedimento P ON  P.id_procedimento=AP.id_procedimento
+                        INNER JOIN empresa E ON E.id_empresa=E.id_empresa
+                        WHERE  A.data BETWEEN '".$data_inicio."' and '".$data_fim."'
+                        ORDER BY E.nome_empresa ASC, A.data ASC;";
+                        $result = mysqli_query($con,$query);
+                        while($linha = mysqli_fetch_assoc($result)){ ?>
                         <tr>
                             <td>
-                                <?= $consulta['nome_empresa']; ?>
+                                <?= $linha['nome_paciente']; ?>
                             </td>
                             <td>
-                                <?= $consulta['nome_paciente']; ?>
+                                <?= $linha['nome_procedimento']; ?>
                             </td>
                             <td>
-                            <?= $consulta['nome_medico']; ?>
+                            <?= $linha['valor']; ?>
                             </td>
                             <td>
-                                <?= $consulta['tipo_exame']; ?>
+                                <?= $linha['data']; ?>
                             </td>
                             <td>
-                            <?= $consulta['hora_checkin']; ?>
-                            </td>
-                            <td>
-                            <a href='cadastrar_atendimento.php?finalizar=true&id_atendimento=<?= $consulta['id_atendimento']; ?>'>FINALIZAR</a>
+                                <?= $linha['nome_empresa']; ?>
                             </td>
                         </tr>
                     <?php //FECHANDO WHILE
