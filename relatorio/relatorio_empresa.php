5efd4147886e649f06097d4ef00a94dc76e010ff
HamadaTecnologias <hamada.st@hotmail.com>
2024-03-30 12:39:05 -0300

    feat:

--- relatorio/relatorio_empresa.php
+++ relatorio/relatorio_empresa.php
@@ -1,68 +1,83 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="script.js" defer></script>
     <link rel="shortcut icon" href="../assets/crvital-logo.svg" type="image/x-icon">
     <link rel="stylesheet" href="../assets/css/relatorio_empresa.css">
     <title>Relatório Empresa</title>
-</head>
-<body>            
+</head>        
     <?php 
         include "../bd_connect.php";
         $data_inicio = $_GET['data_inicio'];
         $data_fim = $_GET['data_fim'];
         $id_empresa = $_GET['id_empresa'];
+        $valor_empresa = 0;
         $error=FALSE;
-        if ($id_empresa == null) {
+        if (empty($id_empresa)) {
             $error=TRUE;
+        }
+        if(empty($data_inicio)){
+            $erro=TRUE;
+            echo $data_inicio;
+        }
+        if(empty($data_fim)){
+            $erro=TRUE;
+            echo $data_fim;
         }
         if ($error!=FALSE) {
             header('location:relatorio.php?error=true');
         }        
     ?>
 
-    <div id="content">
+  
 
     <?php
+        //SELEÇÃO DA DATA DE EMISSÃO
         $query_data = "SELECT DATE_FORMAT(CURDATE(), '%d/%m/%Y')";
         $result_data = mysqli_query($con,$query_data);
         $data = mysqli_fetch_assoc($result_data);
         $data_emissao = $data['DATE_FORMAT(CURDATE(), \'%d/%m/%Y\')'];
 
+        //SOMATÓRIO DO VALOR DE TODOS OS EXAMES POR PERÍODO
+        $query_valor_empresa = 
+        "SELECT P.valor 
+        FROM atendimento A
+        INNER JOIN atendimento_procedimento AP ON A.id_atendimento=AP.id_atendimento
+        INNER JOIN procedimento P ON P.id_procedimento=AP.id_procedimento
+        INNER JOIN empresa E ON E.id_empresa=P.id_empresa
+        WHERE A.data BETWEEN '".$data_inicio."' and '".$data_fim."' AND  A.id_empresa=".$id_empresa;  
+        $result_valor_empresa = mysqli_query($con,$query_valor_empresa);
+        while($linha_valor_empresa = mysqli_fetch_assoc($result_valor_empresa)){
+            $valor_empresa = $valor_empresa + $linha_valor_empresa['valor'];
+        }
+        //QUERY DADOS DA EMPRESA
+        $query_dados_empresa="SELECT * FROM empresa WHERE id_empresa = ".$id_empresa.";";
+        $result_empresa = mysqli_query($con,$query_dados_empresa);
+        $linha_empresa = mysqli_fetch_assoc($result_empresa);
+        $cnpj = $linha_empresa['cnpj'];
+        $cnpj_formatado = substr($cnpj, 0, 2) . '.' . substr($cnpj, 2, 3) . '.' . substr($cnpj, 5, 3) . '/' . substr($cnpj, 8, 4) . '-' . substr($cnpj, 12, 2);
+       
     ?>
-
+<body>    
     <header>
         <div class="img">
             <img src="../assets/logo-crvital-horizontal.png" alt="logo">
         </div>  
         <div class="titulo-header">
-            <h2>Período de: <?= date('d/m/Y', strtotime($data_inicio)); ?> à <?= date('d/m/Y', strtotime($data_fim)); ?></h2>
+            <h2><strong>Empresa: <?= strtoupper($linha_empresa['nome_empresa']); ?></strong>
+            <h4><strong>CNPJ:</strong> <?= $cnpj_formatado; ?></h2>
             <div class="subtitulo-header" style="text-align: center;">
-                <p><strong>Data de Emissão:</strong></p>
-                <p><?= $data_emissao ?></p> 
+                <p><strong>Perfil: </strong><?= strtoupper($linha_empresa['perfil']); ?></p>
+                <p><strong>Método de Pagamento: </strong><?= strtoupper($linha_empresa['forma_pagamento']); ?></p>
+                    <h4>Emitido em: </h4>
+                    <?=$data_emissao?>
+            </div>
+            <div class="valor_geral">
+                <?="O valor total dos procedimentos do período é: R$ ".$valor_empresa?>
             </div>
         </div>
     </header>
-
-
-    <div class="main-empresa">
-        <?php
-            $query="SELECT * FROM empresa WHERE id_empresa = ".$id_empresa.";";
-            $result = mysqli_query($con,$query);
-            $linha = mysqli_fetch_assoc($result);
-            $cnpj = $linha['cnpj'];
-            $cnpj_formatado = substr($cnpj, 0, 2) . '.' . substr($cnpj, 2, 3) . '.' . substr($cnpj, 5, 3) . '/' . substr($cnpj, 8, 4) . '-' . substr($cnpj, 12, 2);
-        ?>
-        <div class="empresa">
-            <h2><strong>Empresa: <?= strtoupper($linha['nome_empresa']); ?></strong></h2>
-            <h3><strong>CNPJ:</strong> <?= $cnpj_formatado; ?></h3>
-            <p><strong>Perfil: </strong><?= strtoupper($linha['perfil']); ?></p>
-            <p><strong>Método de Pagamento: </strong><?= strtoupper($linha['forma_pagamento']); ?></p>
-        </div>
-    </div>
 
     <table>
             <thead>
@@ -129,6 +144,7 @@
                 ?>  
             </tbody>
     </table>
-    <script>window.print();</script>
+
+<!-- <script>window.print();</script> -->
 </body>
 </html>
