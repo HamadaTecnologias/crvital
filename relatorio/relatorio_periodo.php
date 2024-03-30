e35af2d796c6ab968de80a112bb3db717540c1e3
HamadaTecnologias <hamada.st@hotmail.com>
2024-03-30 10:59:58 -0300

    feat: soma-valores-procedimentos

--- relatorio/relatorio_periodo.php
+++ relatorio/relatorio_periodo.php
@@ -12,6 +12,7 @@
         include "../bd_connect.php";       
         $data_inicio = $_GET['data_inicio'];
         $data_fim = $_GET['data_fim'];
+        $valor_geral = 0;
         $erro = FALSE;
         if(empty($data_inicio)){
             $erro=TRUE;
@@ -29,8 +30,21 @@
         $result_data = mysqli_query($con,$query_data);
         $data = mysqli_fetch_assoc($result_data);
         $data_emissao = $data["DATE_FORMAT(CURDATE(), '%d/%m/%Y')"];
-          
+
+        //SOMATÓRIO DO VALOR DE TODOS OS EXAMES POR PERÍODO
+        $query_valor_geral = 
+        "SELECT P.valor 
+        FROM atendimento A
+        INNER JOIN atendimento_procedimento AP ON A.id_atendimento=AP.id_atendimento
+        INNER JOIN procedimento P ON P.id_procedimento=AP.id_procedimento
+        INNER JOIN empresa E ON E.id_empresa=P.id_empresa
+        WHERE A.data BETWEEN '".$data_inicio."' and '".$data_fim."'";  
+        $result_valor_geral = mysqli_query($con,$query_valor_geral);
+        while($linha_valor_geral = mysqli_fetch_assoc($result_valor_geral)){
+            $valor_geral = $valor_geral + $linha_valor_geral['valor'];
+        }                                                        
     ?>
+
 <body>
 
     <header>
@@ -38,12 +52,14 @@
         <img src="../assets/logo-crvital-horizontal.png" alt="logo">
     </div>  
     <div class="titulo-header">
-           <h2>Período de: 
-        <?= date('d/m/Y', strtotime($data_inicio)); ?> à <?= date('d/m/Y', strtotime($data_fim)); ?></h2>
+            <h2>Período de: <?= date('d/m/Y', strtotime($data_inicio)); ?> à <?= date('d/m/Y', strtotime($data_fim)); ?></h2>           
             <div class="subtitulo-header">
-            <h4>Relação por período</h4>
-            <h4>Emitido em: </h4>
-            <?=$data_emissao?>
+                <h4>Relação por período</h4>
+                <h4>Emitido em: </h4>
+                <?=$data_emissao?>
+            </div>
+            <div class="valor_geral">
+                <?="O valor total dos procedimentos do período é: R$ ".$valor_geral?>
             </div>
     </div>
     </header>
@@ -71,12 +87,27 @@
                         $ids_empresa = array();
                         while($linha = mysqli_fetch_assoc($result)){ 
                             
+                            
                             if (!in_array($linha['id_atendimento'], $ids_atendimento)) {
                                 if (!in_array($linha['id_empresa'], $ids_empresa)) {
                                     $cnpj = formatCnpjCpf($linha['cnpj']);
+                                    //VALOR DOS PROCEDIMENTOS POR EMPRESA
+                                    $valor_empresa = 0;
+                                    $query_valor_empresa = "SELECT P.valor 
+                                    FROM atendimento A
+                                    INNER JOIN atendimento_procedimento AP ON A.id_atendimento=AP.id_atendimento
+                                    INNER JOIN procedimento P ON P.id_procedimento=AP.id_procedimento
+                                    INNER JOIN empresa E ON E.id_empresa=P.id_empresa
+                                    WHERE A.data BETWEEN '".$data_inicio."' and '".$data_fim."' AND  A.id_empresa=".$linha['id_empresa'];  
+                                    $result_valor_empresa = mysqli_query($con,$query_valor_empresa);
+                                    while($linha_valor_empresa = mysqli_fetch_assoc($result_valor_empresa)){
+                                        $valor_empresa = $valor_empresa + $linha_valor_empresa['valor'];
+                                    }
 
-                                    echo"<th>".$linha['nome_empresa']."</th>";
+
+                                    echo"<th class='quebra'>".$linha['nome_empresa']."</th>";
                                     echo"<th> CNPJ: ".$cnpj."</th>";
+                                    echo"<th>R$ ".$valor_empresa."</th>";
                                     array_push($ids_empresa,$linha['id_empresa']);
                                 }
                     ?>          
@@ -112,14 +143,15 @@
                                         }?>
                                     </td>
                                     <td>
-                                        <?= $linha['data']; ?>
+                                        <?= date('d/m/Y', strtotime($linha['data'])); ?>
                                     </td> 
-                                </tr>
+                                </tr>                             
+                                
                     <?php 
                             } //FECHANDO O IF
-                            array_push($ids_atendimento,$linha['id_atendimento']);
+                            array_push($ids_atendimento,$linha['id_atendimento']);                   
                         } //FECHANDO WHILE
-                    ?>  
+                    ?>   
                 </tbody>
             </table> 
 <script>window.print();</script>
