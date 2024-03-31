73cd37a3d2b3641a5f1edee69295fb1337bf72d2
Juliana Gagliano <julianagagliano1@gmail.com>
2024-03-31 17:05:12 -0300

    fix file name

--- relatorio/relatorio.php
+++ relatorio/relatorio.php
@@ -16,8 +16,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../assets/logo-favicon.png" type="image/x-icon">
     <link rel="stylesheet" href="../assets/css/sidebar.css">
-    <!-- <link rel="stylesheet" href="../assets/css/relatorio.css"> -->
-    <link rel="stylesheet" href="../assets/css/relatorio-novo.css">
+    <link rel="stylesheet" href="../assets/css/relatorio.css">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
