c350d7f398cf2574eee5e336c1eef821f8e32d39
HamadaTecnologias <hamada.st@hotmail.com>
2024-03-31 15:19:53 -0300

    feat: mudando-favicon

--- relatorio/relatorio_medico.php
+++ relatorio/relatorio_medico.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link rel="shortcut icon" href="../assets/logo-favicon.png" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/relatorio_medico.css">
     <title>Relatório Médico</title>
--- relatorio/relatorio_periodo.php
+++ relatorio/relatorio_periodo.php
@@ -2,6 +2,7 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <link rel="shortcut icon" href="../assets/logo-favicon.png" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/relatorio_periodo.css">
     <title>Relatório por período</title>
