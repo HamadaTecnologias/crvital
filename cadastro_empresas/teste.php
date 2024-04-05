93c6a4d689a8f0fc015aee439300ef645b3d2e53
HamadaTecnologias <hamada.st@hotmail.com>
2024-04-05 00:45:30 -0300

    fix: tratamento-cadastro-empresa

---
+++ cadastro_empresas/teste.php
@@ -0,0 +1,10 @@
+<?php 
+    $id_empresa=$_POST["id_empresa"]??null;
+    // $alterar = $_POST['alterar']??false;
+    $erro = FALSE;
+    include "../bd_connect.php";
+
+    echo $id_empresa;
+
+
+?>
